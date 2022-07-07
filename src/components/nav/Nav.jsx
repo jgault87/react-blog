8d45b601246295eb655ccd4748bbac672c44e09e
jgault87 <99287734+jgault87@users.noreply.github.com>
2022-07-07 14:15:12 -0600

    page layout for components and react icons

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import './nav.css'
+import React from 'react';
+import './nav.css';
+import { AiOutlineHome } from 'react-icons/ai';
+import { FaUserAstronaut } from 'react-icons/fa';
+import { BiBook } from 'react-icons/bi';
+import { RiServiceLine } from 'react-icons/ri';
+import { BiMessageSquareDetail } from 'react-icons/bi';
 
 const Nav = () => {
   return (
-    <div>Nav</div>
-  )
-}
+    <nav>
+      <a href='#'>
+        <AiOutlineHome />
+      </a>
+      <a href='#about'>
+        <FaUserAstronaut />
+      </a>
+      <a href='#experience'>
+        <BiBook />
+      </a>
+      <a href='#services'>
+        <RiServiceLine />
+      </a>
+      <a href='#contact'>
+        <BiMessageSquareDetail />
+      </a>
+    </nav>
+  );
+};
 
-export default Nav
+export default Nav;
