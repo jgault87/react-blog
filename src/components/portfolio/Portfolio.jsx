6a5152deec8387974a344ca49bd6897b536be8a9
jgault87 <99287734+jgault87@users.noreply.github.com>
2022-07-08 15:20:27 -0600

    added desc to portfolio array

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -13,6 +13,8 @@ const data = [
     title: 'Teacup Yorkies (Full-Stack Application)',
     github: 'https://github.com/jgault87/teacup-yorkies',
     demo: 'https://howler-forpets.herokuapp.com',
+    description:
+      "This application is a collaboratively developed social media application using the full-stack MVC paradigm. The concept is a social media website for pet-oriented content and showcasing a user's pet with pictures, traits and personality. It's also a place for interacting with other pet owners. We like to think of it as a twitter-like website for our furry friends. Read more about the technologies used on Github and try it out on Heroku!",
   },
   {
     id: 2,
@@ -20,6 +22,8 @@ const data = [
     title: 'Blog Buster (Full-stack application)',
     github: 'https://github.com/jgault87/blog-buster',
     demo: 'https://mighty-savannah-09972.herokuapp.com/',
+    description:
+      'this project is my first endeavor into creating a full-stack application that makes use of front end, back end and database techs that culminate in a tech blog website that verifies user credentials, uses cookies and session data to handle user queries and UI is affected by user states. It uses bcrypt hooks to hash password data, handlebars webviews engine and Sequelize to hand the SQL database. Visit the Github page to learn what other technologies/languages were used and the deployed link to heroku!',
   },
   {
     id: 3,
@@ -27,6 +31,8 @@ const data = [
     title: 'Weather Machine',
     github: 'https://github.com/jgault87/weather-machine',
     demo: 'https://jgault87.github.io/weather-machine/',
+    description:
+      'This project is a weather dashboard that calls the One Call open weather API and dynamically generates weather forecast, UVI index data, saves previous searches in local browser storage.',
   },
   {
     id: 4,
@@ -34,6 +40,8 @@ const data = [
     title: 'Team Visualizer',
     github: 'https://github.com/jgault87/team_visualizer',
     demo: 'https://github.com/jgault87/team_visualizer',
+    description:
+      'This is a handy node application which makes use of inquirer prompts to capture user input and dynamically generate an HTML page with profile information of the following individual team member roles: Manager, Engineer and Intern. It extends javaScript classes to pass along inherited properties which will then parse the information to an HTML file which makes use of Materialize CSS framework so that all of the styling and script is included in the HTML file.',
   },
   {
     id: 5,
@@ -41,6 +49,8 @@ const data = [
     title: 'Generate readYou',
     github: 'https://github.com/jgault87/generateReadYou',
     demo: 'https://github.com/jgault87/generateReadYou',
+    description:
+      'this application uses node.js and inquirer to capture responses in the terminal and then output them into a readme file with a traversable interface and organization. You can use this to generate a readme for any application using the markdown language.',
   },
 ];
 
