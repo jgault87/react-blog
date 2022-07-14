b4a5320bea6630923a893f65c85157438bfd1bc1
jgault87 <99287734+jgault87@users.noreply.github.com>
2022-07-14 11:52:18 -0600

    projects

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -71,7 +71,7 @@ const Portfolio = () => {
     
     <section id='portfolio'>
       <h5>My Recent Work</h5>
-      <h2>Portfolio</h2>
+      <h2>Projects</h2>
       <div className='container portfolio__container'>
         {
         projects.map(({ id, image, title, github, demo }) => {
